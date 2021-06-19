77ac3c2954a835dd9d730498483764a7e4ac018f
AdiAharonov <adi.ahronov@gmail.com>
2021-06-19 21:54:13 +0300

    Updates

---
+++ precache-manifest.df4e28987706cab4de02ee26517f9fbf.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3888becc11acaa3e53c76e4258ad4466",
+    "url": "/manager-tools-client/index.html"
+  },
+  {
+    "revision": "d0fac2c0a62727af9eac",
+    "url": "/manager-tools-client/static/css/main.399a400f.chunk.css"
+  },
+  {
+    "revision": "29fdfc97a6325b47fd89",
+    "url": "/manager-tools-client/static/js/2.67856d0d.chunk.js"
+  },
+  {
+    "revision": "dcd5929c94b5e05a934eed2a8abf4e57",
+    "url": "/manager-tools-client/static/js/2.67856d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fac2c0a62727af9eac",
+    "url": "/manager-tools-client/static/js/main.aa846166.chunk.js"
+  },
+  {
+    "revision": "d089809b39ae883a895d",
+    "url": "/manager-tools-client/static/js/runtime-main.5bb3d8c7.js"
+  },
+  {
+    "revision": "c43448c2f851a4af0d0e93dd714bf95b",
+    "url": "/manager-tools-client/static/media/Playfair-Bold.c43448c2.ttf"
+  },
+  {
+    "revision": "9340b57ab62995c2c184efbfe620991c",
+    "url": "/manager-tools-client/static/media/Solway-Bold.9340b57a.ttf"
+  },
+  {
+    "revision": "0314833123d831ab7fea21d772112343",
+    "url": "/manager-tools-client/static/media/Solway-Regular.03148331.ttf"
+  },
+  {
+    "revision": "36b1086313422fba09e61a508d14a666",
+    "url": "/manager-tools-client/static/media/add-layer.36b10863.svg"
+  },
+  {
+    "revision": "0338decbdbdfc711d7b22112010dc194",
+    "url": "/manager-tools-client/static/media/export-image.0338decb.svg"
+  },
+  {
+    "revision": "125ddbe009dcaea3f89af01c7cf576b8",
+    "url": "/manager-tools-client/static/media/eye-hidden.125ddbe0.svg"
+  },
+  {
+    "revision": "4e5162defd496897460eb36fa46f071f",
+    "url": "/manager-tools-client/static/media/eye.4e5162de.svg"
+  },
+  {
+    "revision": "3fdca61e21fe8b0490ae33262525647f",
+    "url": "/manager-tools-client/static/media/grid-hidden.3fdca61e.svg"
+  },
+  {
+    "revision": "7b9a4bbda1af1d93791b3ac9976b77c5",
+    "url": "/manager-tools-client/static/media/grid.7b9a4bbd.svg"
+  },
+  {
+    "revision": "0304b7aa721096417a8b273f840f704e",
+    "url": "/manager-tools-client/static/media/item.0304b7aa.svg"
+  },
+  {
+    "revision": "90811d5b43c8954ec5e5ac1095952440",
+    "url": "/manager-tools-client/static/media/landing-page.90811d5b.png"
+  },
+  {
+    "revision": "998f4c04dbb3405168b27bd63c817982",
+    "url": "/manager-tools-client/static/media/layers.998f4c04.svg"
+  },
+  {
+    "revision": "d409a1891ff86faa189a07ff3c746b1f",
+    "url": "/manager-tools-client/static/media/minus.d409a189.svg"
+  },
+  {
+    "revision": "20bc699307ad9a48285152bdbbaa3866",
+    "url": "/manager-tools-client/static/media/pen.20bc6993.svg"
+  },
+  {
+    "revision": "610065eff1e85cee21b85953fcd43b4b",
+    "url": "/manager-tools-client/static/media/plus.610065ef.svg"
+  },
+  {
+    "revision": "8e098fa1e1441a22cfdfec25a57800c3",
+    "url": "/manager-tools-client/static/media/rect.8e098fa1.svg"
+  },
+  {
+    "revision": "dda7cb68579a30cb25f6611bb10fe848",
+    "url": "/manager-tools-client/static/media/redo.dda7cb68.svg"
+  },
+  {
+    "revision": "6b447420a00079383fe6e5b461faf526",
+    "url": "/manager-tools-client/static/media/rotate-left.6b447420.svg"
+  },
+  {
+    "revision": "03261172fb10278911a5ebd526a5300d",
+    "url": "/manager-tools-client/static/media/rotate-right.03261172.svg"
+  },
+  {
+    "revision": "3ed0f22723d47f136172a084fcc08508",
+    "url": "/manager-tools-client/static/media/undo.3ed0f227.svg"
+  },
+  {
+    "revision": "94c15670483ec4f8a08d42c4c1c06df6",
+    "url": "/manager-tools-client/static/media/upload-background-image.94c15670.svg"
+  }
+]);
